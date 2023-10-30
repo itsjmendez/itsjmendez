7b7f7024cee252989efe57cb65db4b2c1da4c6d0
Josemanuel Mendez <jmocontactme@gmail.com>
2023-10-29 23:12:12 -0700

    Add: geist font configuration

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
+import { GeistSans, GeistMono } from 'geist/font';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -10,7 +11,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
       <body className={inter.className}>{children}</body>
     </html>
   );
